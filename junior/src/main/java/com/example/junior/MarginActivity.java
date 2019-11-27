bdeced65b83ee5f5b09b7bc9cf4dce87a6f369c3
aqi00 <aqi00@163.com>
2019-11-27 10:07:37 +0800

    第2章 初级控件

---
+++ junior/src/main/java/com/example/junior/MarginActivity.java
@@ -0,0 +1,17 @@
+package com.example.junior;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by ouyangshen on 2017/9/14.
+ */
+public class MarginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_margin);
+    }
+}
+
