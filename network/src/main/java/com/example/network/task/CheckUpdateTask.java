e53df09a8709867d08deff9ad9a2ad0967bffb6f
aqi00 <aqi00@163.com>
2022-08-08 15:07:59 +0800

    第10章  网络通信

--- network/src/main/java/com/example/network/task/CheckUpdateTask.java
+++ network/src/main/java/com/example/network/task/CheckUpdateTask.java
@@ -28,14 +28,14 @@ public class CheckUpdateTask extends AsyncTask<String, Void, String> {
         HttpReqData req_data = new HttpReqData(mQueryUrl);
         req_data.params.append(params[0]);
         // 发送HTTP请求信息，并获得HTTP应答对象
-//        HttpRespData resp_data = HttpRequestUtil.postData(req_data);
+        HttpRespData resp_data = HttpRequestUtil.postData(req_data);
 //        try {
 //            Thread.sleep(1500);
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
-        HttpRespData resp_data = new HttpRespData();
-        resp_data.content = "{\"package_list\":[{\"new_version\":\"10.9.0\",\"download_url\":\"https://3g.lenovomm.com/w3g/yydownload/com.qiyi.video/60020\",\"package_name\":\"com.qiyi.video\"},{\"new_version\":\"8.9.2\",\"download_url\":\"https://3g.lenovomm.com/w3g/yydownload/com.kugou.android/60020\",\"package_name\":\"com.kugou.android\"},{\"new_version\":\"7.0.5.0\",\"download_url\":\"https://3g.lenovomm.com/w3g/yydownload/com.mt.mtxx.mtxx/60020\",\"package_name\":\"com.mt.mtxx.mtxx\"},{\"new_version\":\"6.6.1\",\"download_url\":\"https://3g.lenovomm.com/w3g/yydownload/com.tencent.mm/60020\",\"package_name\":\"com.tencent.mm\"},{\"new_version\":\"7.4.0\",\"download_url\":\"https://3g.lenovomm.com/w3g/yydownload/com.taobao.taobao/60020\",\"package_name\":\"com.taobao.taobao\"},{\"new_version\":\"7.3.2\",\"download_url\":\"https://3g.lenovomm.com/w3g/yydownload/com.tencent.mobileqq/60020\",\"package_name\":\"com.tencent.mobileqq\"}]}";
+//        HttpRespData resp_data = new HttpRespData();
+//        resp_data.content = "{\"package_list\":[{\"new_version\":\"13.7.5\",\"download_url\":\"https://imtt.dd.qq.com/sjy.10001/sjy.00004/16891/apk/F1E6E8850F6F80BD071AE8E319C51759.apk\",\"package_name\":\"com.qiyi.video\"},{\"new_version\":\"11.2.6\",\"download_url\":\"https://imtt.dd.qq.com/sjy.10001/sjy.00004/16891/apk/52C4F042C7AC91FE1D97EE8699E0191C.apk\",\"package_name\":\"com.kugou.android\"},{\"new_version\":\"9.6.2.0\",\"download_url\":\"https://imtt.dd.qq.com/sjy.10001/sjy.00004/16891/apk/FAE5E860689E67BAEE7038261614B64E.apk\",\"package_name\":\"com.mt.mtxx.mtxx\"},{\"new_version\":\"8.0.25\",\"download_url\":\"https://imtt.dd.qq.com/sjy.10001/sjy.00004/16891/apk/5F93793C6C5F539487B11418A5D4C902.apk\",\"package_name\":\"com.tencent.mm\"},{\"new_version\":\"10.15.0\",\"download_url\":\"https://imtt.dd.qq.com/sjy.10001/sjy.00004/16891/apk/99CA4F060773B985797B9EFA0DA0A632.apk\",\"package_name\":\"com.taobao.taobao\"},{\"new_version\":\"8.9.3\",\"download_url\":\"https://imtt.dd.qq.com/sjy.10001/sjy.00004/16891/apk/995F74DF2D12C2325158455865E4CC4C.apk\",\"package_name\":\"com.tencent.mobileqq\"}]}";
         Log.d(TAG, "err_msg=" + resp_data.err_msg + ", result=" + resp_data.content);
         return resp_data.content; // 返回HTTP调用的应答内容
     }
